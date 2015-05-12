304478248cd591a770e59bee02bf3ff7e6d4a145
rwlumino <reubenwakefield@gmail.com>
2015-05-12 13:13:14 +1000

    Commenting + Function change
    Added description comments on each line
    Changed .click function to be .on( "click" )
    Changed "spin180" class to be "down" to fit with transformer.css

--- js/hider.js
+++ js/hider.js
@@ -1,16 +1,19 @@
 //	//	//	//	//	//	//	//	//	//	//	//	//	//
 //													//
-//	Show-More Plugin by Reuben @dalegroup	       	//
+//	Hider Plugin by Reuben @dalegroup	          	//
 //													//
 //	//	//	//	//	//	//	//	//	//	//	//	//	//
+
+
+
 $(document).ready(function() {
-    $('.show-more').attr('title', 'Press to view more');
-    $('a.show-more').attr('href', '#');
-    $('.show-more').click(function() {
-		$(this).closest('div').find('.hide-this').slideToggle();
-        $(this).closest('div').find('.hide-this-notoggle').slideDown();
-        $(this).closest('div').find('.hide-this-mobile').slideDown();
-        $(this).closest('div').find('.hide-arrow').toggleClass('spin180');
-        $(this).closest('div').find('.more-text').html($('.more-text').text() == 'Show less options' ? 'Show more options' : 'Show less options');
+    //Show-Hide script  
+    $('.show-more').attr('title', 'Press to view more');    // Add this to the show button/link
+    $('a.show-more').attr('href', '#'); // Clears anchors from the show/hide button
+		$(this).closest('div').find('.hide-this').slideToggle();  //Use .hide-this on a div adjacent to the .show-more button to slideToggle it on click
+        $(this).closest('div').find('.hide-this-notoggle').slideDown(); // Only appears once, clicking again doesnt hide
+        $(this).closest('div').find('.hide-this-mobile').slideDown(); //This element is only hidden on mobile devices
+        $(this).closest('div').find('.hide-arrow').toggleClass('down'); //If .hide-arrow is used, rotates it 180 degrees
+        $(this).closest('div').find('.more-text').html($('.more-text').text() == 'Show less options' ? 'Show more options' : 'Show less options');  //Changes hide/show button text
     });
 });
